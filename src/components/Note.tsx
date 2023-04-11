e885d27489f020dc26324f69bb09c7bf22c4b879
khinsuwai97 <khinsuwai009@gmail.com>
2023-04-11 20:20:15 +0630

    chaded css

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, Navigate } from 'react-router-dom';
 import Button from './Button';
 import { useNote } from './NoteLayout';
 import useNoteContext from '../hooks/useNoteContext';
@@ -8,16 +8,20 @@ const Note = () => {
   const navigate = useNavigate();
   const { deleteNote } = useNoteContext();
 
+  if (!note) {
+    return <Navigate to="/" replace />;
+  }
+
   return (
     <section className="container">
-      <div className="mb-10 flex sm:flex-row flex-col-reverse justify-between ">
+      <div className="mb-10 flex md:flex-row flex-col-reverse justify-between ">
         <div>
           <h1 className="text-header sm:mt-0 mt-4 sm:mb-8 mb-4">
             {note.title}
           </h1>
           <p className="text-primary">{note.markdown}</p>
         </div>
-        <div>
+        <div className="sm:mb-2 mb-0">
           <Link to={`/${note.id}/edit`}>
             <button className="text-button outline-none  cursor-pointer rounded-[5px] whitespace-nowrap bg-secondary px-[15px] py-[8px] mr-2">
               Edit
